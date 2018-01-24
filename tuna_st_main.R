86eaaaef55f9d2b6cde13dd12099b1a396542b18
Peter Kuriyama <peter.kuriyama@nmfs.local>
2018-01-24 09:53:24 -0800

    update script

---
+++ tuna_st_main.R
@@ -0,0 +1,10 @@
+#Set working directory based on computer I'm using
+
+if(Sys.info()[1] == 'Windows') setwd('\\Users\\peter.kuriyama\\Desktop\\tuna_st')
+if(Sys.info()[1] != 'Windows') setwd('/Users/peterkuriyama/Dropbox/postdoc/yellowtail_spatiotemporal')
+
+
+setwd('C:/Windows/Users/peter.kuriyama/Desktop/tuna_st')
+
+setwd('/Users/peterkuriyama/Dropbox/postdoc/yellowtail_spatiotemporal')
+

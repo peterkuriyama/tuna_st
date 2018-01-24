23b34f82af30c5f99434716c3e7570a005b0a290
Peter Kuriyama <peter.kuriyama@nmfs.local>
2018-01-24 10:23:38 -0800

    update script

--- tuna_st_main.R
+++ tuna_st_main.R
@@ -1,10 +1,19 @@
 #Set working directory based on computer I'm using
-
 if(Sys.info()[1] == 'Windows') setwd('\\Users\\peter.kuriyama\\Desktop\\tuna_st')
 if(Sys.info()[1] != 'Windows') setwd('/Users/peterkuriyama/Dropbox/postdoc/yellowtail_spatiotemporal')
 
+#Install packages
+# install.packages("TMB")
 
-setwd('C:/Windows/Users/peter.kuriyama/Desktop/tuna_st')
+# devtools::install_github("james-thorson/VAST",force = TRUE)
+devtools::install_github("james-thorson/utilities",force = TRUE)
+# devtools::install_github("nwfsc-assess/geostatistical_delta-GLMM",force = TRUE)
 
-setwd('/Users/peterkuriyama/Dropbox/postdoc/yellowtail_spatiotemporal')
+library(TMB)
+library(VAST)
+library(SpatialDeltaGLMM)
+
+#Read in Data
+
+
 

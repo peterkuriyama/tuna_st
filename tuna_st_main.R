1d97ef04bd9991702fbe5946dd48764644289f53
peterkuriyama <ptrkrym@uw.edu>
2018-01-24 13:46:24 -0800

    update script

--- tuna_st_main.R
+++ tuna_st_main.R
@@ -1,19 +1,30 @@
+#-----------------------------------------------------------------------------------------------------
 #Set working directory based on computer I'm using
 if(Sys.info()[1] == 'Windows') setwd('\\Users\\peter.kuriyama\\Desktop\\tuna_st')
-if(Sys.info()[1] != 'Windows') setwd('/Users/peterkuriyama/Dropbox/postdoc/yellowtail_spatiotemporal')
+if(Sys.info()[1] != 'Windows') setwd('/Users/peterkuriyama/Dropbox/postdoc/tuna_st')
 
+#-----------------------------------------------------------------------------------------------------
 #Install packages
 # install.packages("TMB")
 
 # devtools::install_github("james-thorson/VAST",force = TRUE)
-devtools::install_github("james-thorson/utilities",force = TRUE)
+# devtools::install_github("james-thorson/utilities",force = TRUE)
 # devtools::install_github("nwfsc-assess/geostatistical_delta-GLMM",force = TRUE)
 
+#Load Packages
 library(TMB)
 library(VAST)
 library(SpatialDeltaGLMM)
 
+#-----------------------------------------------------------------------------------------------------
 #Read in Data
+bet_comps <- read.csv("data/bet_length_comps.csv", stringsAsFactors = FALSE)
+bill_catch <- read.csv("data/PublicLLTunaBillfishMt.csv", stringsAsFactors = FALSE)
+bill_numbers <- read.csv("data/PublicLLTunaBillfishNum.csv", stringsAsFactors = FALSE)
+
+#-----------------------------------------------------------------------------------------------------
+
+
 
 
 

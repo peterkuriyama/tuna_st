080c296f45f0c27912874b16b2141d22e4846bb2
peterkuriyama <ptrkrym@uw.edu>
2018-02-07 16:45:41 -0800

    update script

--- tuna_st_main.R
+++ tuna_st_main.R
@@ -1,6 +1,8 @@
 #-----------------------------------------------------------------------------------------------------
 #Set working directory based on computer I'm using
+setwd("C:\\Users\\Peter\\Dropbox\\postdoc\\tuna_st")
 if(Sys.info()[1] == 'Windows') setwd('\\Users\\peter.kuriyama\\Desktop\\tuna_st')
+
 if(Sys.info()[1] != 'Windows') setwd('/Users/peterkuriyama/Dropbox/postdoc/tuna_st')
 
 #-----------------------------------------------------------------------------------------------------
@@ -19,134 +21,17 @@ library(tidyverse)
 library(SpatialDeltaGLMM)
 library(reshape2)
 source("Grid_Fn.R")
+source("process_bigeye_data.R")
 #-----------------------------------------------------------------------------------------------------
-#Read in Data
-bill_catch <- read.csv("data/PublicLLTunaBillfishMt.csv", stringsAsFactors = FALSE)
-bill_numbers <- read.csv("data/PublicLLTunaBillfishNum.csv", stringsAsFactors = FALSE)
+#Load the Data
+the_data <- process_bigeye_data(nspp = 3)
 
-#Filter the billfish data to keep bigeye from Japan dat
-bet_catch <- bill_catch %>% filter(Flag == "JPN") %>% select(Year, Month, Flag,
-  LatC5, LonC5, Hooks, BETn, BETmt)
-bet_numbers <- bill_numbers %>% filter(Flag == "JPN") %>% select(Year, Month, Flag,
-  LatC5, LonC5, Hooks, BETn, BETmt)
+#Check the proportion zeroes in the data
+the_data[[2]] %>% group_by(spp, year, lat, lon) %>% summarize(nzeroes = length(which(cpue == 0))) %>% 
+  ungroup %>% distinct(nzeroes)
+hist(the_data[[2]]$cpue, breaks = 50)
 
-#Change column formatting
-bet_numbers <- plyr::rename(bet_numbers, c("LatC5" = "Lat", "LonC5" = "Lon"))
-names(bet_numbers) <- tolower(names(bet_numbers))
-
-#Calculate annual values
-bet_numbers_annual <- bet_numbers %>% group_by(year, lat, lon) %>% 
-  summarize(hooks = sum(hooks), betn = sum(betn)) %>% as.data.frame
-
-bet_numbers_complete <- bet_numbers_annual %>% complete(year, nesting(lat, lon), 
-  fill = list(hooks = 0, betn = 0)) 
-
-#Check proportion of zeroes
-bet_numbers_complete %>% group_by(year) %>% 
-  summarize(zeroes = length(which(betn == 0)),
-    nrowz = length(betn), prop_zero = zeroes / nrowz) %>% tail
-bet_numbers_complete$cpue <- bet_numbers_complete$betn
-bet_numbers_complete$spp <- 1
-bet_numbers_annual <- bet_numbers_complete
-
-###plot the data to see what things look like
-world_map <- map_data("world")
-plot_numbers <- bet_numbers_annual %>% group_by(lat, lon) %>% summarize(cpue = sum(cpue)) 
-
-ggplot() + geom_raster(data = plot_numbers, aes(x = lon, y = lat, fill = cpue)) + 
-  scale_fill_gradient(low = 'white', high = 'red') +
-  geom_map(data = world_map, map = world_map,
-    aes(x = long, y = lat, map_id = region)) + 
-  scale_x_continuous(limits = c(-148, -72)) + 
-  scale_y_continuous(limits = c(-43, 48)) + theme_bw()
-  
-# bet_complete <- bet_co_annual %>% complete(spp, nesting(lat, lon, year),
-#   fill = list(cpue = 0)) %>% as.data.frame
-
-#---------------------------------
-#Composition data
-bet_comps <- read.csv("data/bet_length_comps.csv", stringsAsFactors = FALSE)
-
-#Expand the comps for plotting purposes
-expd_inds <- as.data.frame(bet_comps$Freq)
-expd_inds$ind <- 1:nrow(expd_inds)
-expd_inds <- rep(expd_inds$ind, expd_inds[, 1])
-
-bet_comps_expd <- bet_comps[expd_inds, ]
-
-#**assign 'spp' values for different length classes**#
-#Specify number of length classes here, using 3 now
-nspp <- 1
-bet_comps_expd$spp <- ntile(bet_comps_expd$Bin, nspp)
-names(bet_comps_expd) <- tolower(names(bet_comps_expd))
-
-the_bins <- bet_comps_expd %>% group_by(spp) %>% summarize(mins = round(min(bin), -1), 
-  maxes = round(max(bin), -1)) %>% as.data.frame
-
-#reclassify the species
-for(ii in the_bins$spp){
-  #First bin
-  if(ii == 1){
-    bet_comps_expd[which(bet_comps_expd$bin <= the_bins$maxes[1]), 'spp'] <- 1    
-  }
-
-  if(ii %in% c(1, max(the_bins$spp)) == FALSE){
-    the_inds <- which(bet_comps_expd$bin > the_bins$mins[ii] &
-      bet_comps_expd$bin <= the_bins$maxes[ii])    
-  }
-
-  #Last bin
-  if(ii == max(the_bins$spp)){
-    bet_comps_expd[which(bet_comps_expd$bin > the_bins$maxes[ii]), 'spp'] <- max(the_bins$spp)
-  }
-}
-
-#Treat certain length bins as species
-names(bet_comps_expd) <- tolower(names(bet_comps_expd))
-
-#Change frequency of all rows to 1 (because they are expanded)
-bet_comps_expd$freq <- 1
-sum(bet_comps_expd$freq) == sum(bet_comps$Freq)
-
-#Group on annual or seasonal time scales
-bet_comps_annual <- bet_comps_expd %>% group_by(lat, lon, year,
-  spp) %>% summarize(cpue = sum(freq)) %>% as.data.frame
-bet_comps_seasonal <- bet_comps_expd %>% group_by(lat, lon, year, quarter, spp) %>%
-  summarize(cpue = sum(freq)) %>% as.data.frame
-
-#Complete the data frames by adding zeroes
-bet_complete_annual <- bet_comps_annual %>% complete(spp, nesting(lat, lon, year),
-  fill = list(cpue = 0)) %>% as.data.frame
-bet_complete_seasonal <- bet_comps_seasonal %>% complete(spp, nesting(lat, lon, year, quarter),
-  fill = list(cpue = 0)) %>% as.data.frame
-
-#Look at proportion of zeroes
-bet_complete_annual %>% filter(year >= 1986) %>% group_by( spp, year) %>% summarize(nzeroes = length(which(cpue == 0)),
-  nrows = length(cpue), prop_zeroes = nzeroes / nrows)
-
-bet_complete_seasonal %>% filter(year >= 1986) %>% group_by(spp, year, quarter) %>%
-  summarize(nzeroes = length(which(cpue == 0)), nrows = length(cpue), 
-    prop_zeroes = nzeroes / nrows) %>% ungroup() %>% select(prop_zeroes) %>% unique
-
-#Consider 'CPUE' to be numbers of fish
-#**might make this numbers/nhooks in the future**
-# bet_comps1 <- bet_comps %>% group_by(lat, lon, year, quarter, spp) %>% 
-#   summarize(cpue = sum(freq))
-
-#**Use annual "cpue" for now can also switch to include the quarter columns
-# bet_comps_annual <- bet_comps %>% group_by(lat, lon, year, spp) %>% 
-#   summarize(cpue = sum(freq)) %>% as.data.frame
-
-#**Also filter data so year >= 1990**
-#Just to get model to fit
-# bet_comps_annual <- bet_comps_annual %>% filter(year >= 1990)
-
-#Fill the missing values
-# bet_comps_annual
-
-#Overwrite old version with filled version
-bet_comps_annual <- bet_complete_annual %>% filter(year >= 1986)
-# unique(bet_comps_annual$year)[order(unique(bet_comps_annual$year))]
+head(the_data[[2]])
 
 #-----------------------------------------------------------------------------------------------------
 #VAST Model
@@ -155,20 +40,45 @@ bet_comps_annual <- bet_complete_annual %>% filter(year >= 1986)
 # https://github.com/nwfsc-assess/geostatistical_delta-GLMM/wiki/What-to-do-with-a-species-with-0%25-or-100%25-encounters-in-any-year
 
 #---------------------------------
-#Initial configurations
+#Specify VAST Model Configurations
 Version <- "VAST_v4_0_0"
 Method <- c("Grid", "Mesh", "Spherical_mesh")[2]
 grid_size_km <- 10
-n_x <- c(10, 50, 100, 200, 1000)[1] #number of stations
+n_x <- c(10, 45, 100, 200, 1000)[1] #number of stations
 Kmeans_Config <-  list( "randomseed"=1, "nstart"=100, "iter.max"=1e3 )
 
-#Specify spatial and temporal autocorrelation
-#Fieldconfig is 5 because there are five size categories
-# FieldConfig <- c("Omega1"=5, "Epsilon1"=5, "Omega2"=5, "Epsilon2"=5) 
-FieldConfig <- c("Omega1"=1, "Epsilon1"=1, "Omega2"=1, "Epsilon2"=1) #spatiotemporal variation
+
+#------------FieldConfig
+#Omega refers to spatial variation
+  #Omega1 - variation in encounter probability
+  #Omega2 - variation in positive catch rates; 0 is off
+#Epsilon is spatiotemporal variation
+#AR1 is an AR1 process
+#>0 is number of elements in factor-analysis covariance
+FieldConfig <- c("Omega1"=1, "Epsilon1"=1, "Omega2"=1, "Epsilon2"=1) #
+
+#------------Rhoconfig
+#Specify structure of time intervals (fixed effect or random effect)
+# 1: 
 RhoConfig <- c("Beta1"=0, "Beta2"=0, "Epsilon1"=0, "Epsilon2"=0) #Parameters among years
+
+#------------Overdispersion Config
+#Optional, vector governing correlated overdispersion among categories v_i (vessels)
 OverdispersionConfig <- c("Vessel"=0, "VesselYear"=0)  #Different vessel catchabilities, 0 means they the same
-ObsModel <- c(1, 3) #lognormal disributed then fix the intercept if necessary
+
+#------------ObsModel
+#First element specifies distribution of positive catch rates
+  #0 - normal
+  #1 - lognormal
+  #2 - Gamma
+  #5 - negative binomial
+  #7 - poisson
+#Second element specifies functional form of encounter probabilities
+  #0 - Conventional delta-model, logit-link encounter and log-link positive catch rates
+  #1 - Alternative delta-model, log-link for numbers-density, log-link biomass per number
+  #2 - Link for tweedie observation
+  #3 - Conventional delta-model, fixes encounter probability to 1
+ObsModel <- c(1, 0) 
 
 #Specify options
 Options <- c("SD_site_density"=0, "SD_site_logdensity"=0, "Calculate_Range"=1,
@@ -178,13 +88,90 @@ Options <- c("SD_site_density"=0, "SD_site_logdensity"=0, "Calculate_Range"=1,
 strata.limits <- data.frame('STRATA'="EPO")
 Region <- "Other"
 
-DateFile = paste0(getwd(),"/VAST_output_bet_comp/")
-dir.create(DateFile)
+#Look at proportion of zeroes
+# the_data$bet_numbers_annual %>% group_by(year, lat, lon) 
 
-#Save configurations
-Record = ThorsonUtilities::bundlelist( c("Version","Method","grid_size_km","n_x","FieldConfig","RhoConfig","OverdispersionConfig","ObsModel","Kmeans_Config") )
-save( Record, file=file.path(DateFile,"Record.RData"))
-capture.output( Record, file=paste0(DateFile,"Record.txt"))
+#Look at number of zeroes 
+
+run_vast(dat = the_data$bet_numbers_annual, 
+  ObsModel = c(1, 0))
+
+#---------------------------------
+run_vast <- function(dat, ObsModel){
+
+  #Specify 
+  DateFile = paste0(getwd(),"/VAST_output_", paste0("Obs_", 
+    paste(ObsModel, collapse = "_")))
+  dir.create(DateFile)
+
+  #Save configurations
+  Record = ThorsonUtilities::bundlelist( c("Version","Method","grid_size_km","n_x","FieldConfig","RhoConfig","OverdispersionConfig","ObsModel","Kmeans_Config") )
+  save( Record, file=file.path(DateFile,"Record.RData"))
+  capture.output( Record, file=paste0(DateFile,"Record.txt"))
+
+  #Specify the geostatistical data  
+  Data_Geostat <- data.frame(spp = as.character(dat$spp), 
+                            Catch_num = dat$cpue,
+                            Year = dat$year, 
+                            Vessel = "missing", 
+                            Lat = dat$lat,
+                            AreaSwept_km2 = 1, 
+                            Lon = dat$lon)
+
+  
+  #Specify extrapolation list
+  Extrapolation_List <- Grid_Fn(Data_Geostat = Data_Geostat,
+    zone = 12, grid_dim_km = 10)
+  save(Extrapolation_List, file = paste0(DateFile, "Extrapolation_List.RData"))
+  
+  Spatial_List = Spatial_Information_Fn( grid_size_km = grid_size_km, n_x = n_x, 
+    Method = Method, Lon = Data_Geostat[,'Lon'], Lat = Data_Geostat[,'Lat'], 
+    Extrapolation_List = Extrapolation_List, 
+    randomseed = Kmeans_Config[["randomseed"]], nstart = Kmeans_Config[["nstart"]],  
+   iter.max = Kmeans_Config[["iter.max"]], DirPath = DateFile, Save_Results = FALSE)
+  
+  # Add knots to Data_Geostat
+  Data_Geostat = cbind(Data_Geostat, "knot_i"=Spatial_List$knot_i)
+  # SpatialDeltaGLMM::Plot_data_and_knots(Extrapolation_List = Extrapolation_List,
+  #  Spatial_List = Spatial_List, Data_Geostat = Data_Geostat, PlotDir = DateFile)
+  
+  #---------------------------------
+  #Run the model
+  TmbData = VAST::Data_Fn("Version" = Version, "FieldConfig" = FieldConfig, 
+    "OverdispersionConfig" = OverdispersionConfig,  
+    "RhoConfig" = RhoConfig, "ObsModel" = ObsModel, 
+    "c_i" = as.numeric(Data_Geostat$spp) - 1, 
+    "b_i" = Data_Geostat$Catch_num, 
+    "a_i" = Data_Geostat$AreaSwept_km2, 
+    "v_i" = as.numeric(Data_Geostat$Vessel) - 1, 
+    "s_i" = Data_Geostat$knot_i - 1, 
+    "t_i" = Data_Geostat$Year, 
+    "a_xl" = Spatial_List$a_xl, 
+    "MeshList"=Spatial_List$MeshList, "GridList"=Spatial_List$GridList, 
+    "Method"=Spatial_List$Method, "Options"=Options )
+  
+  # hist(dat$cpue, breaks = 50)
+  
+  TmbList = VAST::Build_TMB_Fn("TmbData" = TmbData, 
+    "RunDir" = DateFile, 
+    "Version" = Version, 
+    "RhoConfig" = RhoConfig, 
+    "loc_x" = Spatial_List$loc_x, "Method" = Method)
+  Obj = TmbList[["Obj"]]
+  
+  Opt = TMBhelper::Optimize(obj = Obj, 
+    newtonsteps = 1,
+    lower = TmbList[["Lower"]], upper = TmbList[["Upper"]], getsd = TRUE, 
+    savedir = DateFile, bias.correct = FALSE)
+  browser()
+}
+
+
+#Specify data and 
+
+
+
+
 
 #---------------------------------
 #Process data
@@ -192,23 +179,11 @@ capture.output( Record, file=paste0(DateFile,"Record.txt"))
 
 dat <- bet_comps_annual
 
-#Check data availability for each location
-distinct()
+
+# For function: Specify data and ObsModel
 
 
-# dat <- bet_numbers_annual
 
-# Error in VAST::Data_Fn(Version = Version, FieldConfig = FieldConfig, OverdispersionConfig = OverdispersionConfig,  : 
-#   Some years and/or categories have either all or no encounters, and this is not permissible when ObsModel_ez[,'Link']=0
-#Check missing values
-
-Data_Geostat <- data.frame(spp = as.character(dat$spp), 
-                          Catch_num = dat$cpue,
-                          Year = dat$year, 
-                          Vessel = "missing", 
-                          Lat = dat$lat,
-                          AreaSwept_km2 = 1, 
-                          Lon = dat$lon)
 
 #Specify extrapolation list
 Extrapolation_List <- Grid_Fn(Data_Geostat = Data_Geostat,
@@ -223,8 +198,8 @@ Spatial_List = Spatial_Information_Fn( grid_size_km = grid_size_km, n_x = n_x,
 
 # Add knots to Data_Geostat
 Data_Geostat = cbind(Data_Geostat, "knot_i"=Spatial_List$knot_i)
-SpatialDeltaGLMM::Plot_data_and_knots(Extrapolation_List = Extrapolation_List,
- Spatial_List = Spatial_List, Data_Geostat = Data_Geostat, PlotDir = DateFile)
+# SpatialDeltaGLMM::Plot_data_and_knots(Extrapolation_List = Extrapolation_List,
+#  Spatial_List = Spatial_List, Data_Geostat = Data_Geostat, PlotDir = DateFile)
 
 #---------------------------------
 #Run the model
@@ -274,7 +249,7 @@ Years2Include = which( Year_Set %in% sort(unique(Data_Geostat[,'Year'])))
 
 residuals <- SpatialDeltaGLMM:::plot_residuals(Lat_i=Data_Geostat[,'Lat'], Lon_i=Data_Geostat[,'Lon'], TmbData=TmbData, Report=Report, Q=Q, savedir=DateFile, MappingDetails=MapDetails_List[["MappingDetails"]], PlotDF=MapDetails_List[["PlotDF"]], MapSizeRatio=MapDetails_List[["MapSizeRatio"]], Xlim=MapDetails_List[["Xlim"]], Ylim=MapDetails_List[["Ylim"]], FileName=DateFile, Year_Set=Year_Set, Years2Include=Years2Include, Rotate=MapDetails_List[["Rotate"]], Cex=MapDetails_List[["Cex"]], Legend=MapDetails_List[["Legend"]], zone=MapDetails_List[["Zone"]], mar=c(0,0,2,0), oma=c(3.5,3.5,0,0), cex=1.8)
 
-SpatialDeltaGLMM::PlotAniso_Fn( FileName=paste0(DateFile,"Aniso.png"), Report=Report, TmbData=TmbData )
+# SpatialDeltaGLMM::PlotAniso_Fn( FileName=paste0(DateFile,"Aniso.png"), Report=Report, TmbData=TmbData )
 
 # # pre <- SpatialDeltaGLMM::PlotResultsOnMap_Fn(plot_set=1, MappingDetails=MapDetails_List[["MappingDetails"]], Report=Report, Sdreport=Opt$SD, PlotDF=MapDetails_List[["PlotDF"]], MapSizeRatio=MapDetails_List[["MapSizeRatio"]], Xlim=MapDetails_List[["Xlim"]], Ylim=MapDetails_List[["Ylim"]], FileName=DateFile, Year_Set=Year_Set, Years2Include=Years2Include, Rotate=MapDetails_List[["Rotate"]], Cex=MapDetails_List[["Cex"]], Legend=MapDetails_List[["Legend"]], zone=MapDetails_List[["Zone"]], mar=c(0,0,2,0), oma=c(3.5,3.5,0,0), cex=1.8, plot_legend_fig=FALSE)
 # # # pos <- SpatialDeltaGLMM::PlotResultsOnMap_Fn(plot_set=2, MappingDetails=MapDetails_List[["MappingDetails"]], Report=Report, Sdreport=Opt$SD, PlotDF=MapDetails_List[["PlotDF"]], MapSizeRatio=MapDetails_List[["MapSizeRatio"]], Xlim=MapDetails_List[["Xlim"]], Ylim=MapDetails_List[["Ylim"]], FileName=DateFile, Year_Set=Year_Set, Years2Include=Years2Include, Rotate=MapDetails_List[["Rotate"]], Cex=MapDetails_List[["Cex"]], Legend=MapDetails_List[["Legend"]], zone=MapDetails_List[["Zone"]], mar=c(0,0,2,0), oma=c(3.5,3.5,0,0), cex=1.8, plot_legend_fig=FALSE)
